288662f73cab732f9aea06087c34ab7f473cae5a
Nathan Wade <nathan.c.wade@gmail.com>
2019-06-20 15:14:12 -0600

    Added test case for Handle()
    Signed-off-by: Nathan Wade <nathan.c.wade@gmail.com>

---
+++ helloworld/handler_test.go
@@ -0,0 +1,13 @@
+package function
+
+import (
+	"testing"
+)
+
+func TestHandle(t *testing.T) {
+	b := []byte("pants")
+    msg := Handle(b)
+    if msg != "Hello, OpenFaaS Cloud. You said: pants" {
+       t.Errorf("Msg was incorrect")
+    }
+}
